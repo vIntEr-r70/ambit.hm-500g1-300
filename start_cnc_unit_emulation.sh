ae407761b55c85ccb261f1ba5c88d23f08960e51
Aleksandr <alexander.kvashnin@gmail.com>
2025-11-05 17:30:09 +0700

    Удалил лишнее

--- start_cnc_unit_emulation.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-~/projects/emulators/CncSlaveUnit/build/cnc-unit /dev/ttySOCAT10
-
--- start_modbus_units_emulation.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ./emulation
-~/projects/emulators/modbus-unit-emulator/build/modbus-unit-emulator /dev/ttySOCAT11 8080
-
